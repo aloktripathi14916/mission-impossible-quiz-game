1cf225e4ffcfe5cb330e1be9c8206525338f32f5
Alok Tripathi <tripathialok.izkn@gmail.com>
2026-05-10 23:01:44 +0530

    Added playable character system with unique abilities

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const welcomeScreen = document.getElementById("welcome-screen");
   const gameScreen = document.getElementById("game-screen");
   const endScreen = document.getElementById("end-screen");
+  const agentDisplay = document.getElementById("agent");
   const livesDisplay = document.getElementById("lives");
   const scoreDisplay = document.getElementById("score");
   const missionNumDisplay = document.getElementById("mission-num");
@@ -18,7 +19,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const historyScreen = document.getElementById("history-screen");
   const historyList = document.getElementById("history-list");
   const closeHistory = document.getElementById("close-history");
+  const characterBtn = document.getElementById("characters");
+  const characterScreen = document.getElementById("character-screen");
+  const closeCharacterScreen = document.getElementById(
+    "close-character-screen",
+  );
+  const selectCharacterBtn = document.querySelectorAll(".select-character");
 
+  let selectedCharacter = "ECHO";
   let missions = [];
   let score = 0;
   let lives = 3;
@@ -40,11 +48,13 @@ document.addEventListener("DOMContentLoaded", () => {
     try {
       const url =
         "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+
       const response = await fetch(url);
       const data = await response.json();
 
       missions = data.results.map((item) => {
         const options = [...item.incorrect_answers, item.correct_answer];
+
         options.sort(() => Math.random() - 0.5);
 
         return {
@@ -65,30 +75,43 @@ document.addEventListener("DOMContentLoaded", () => {
     resetGameVariables();
 
     await fetchMission();
+
     showMission();
     updateDashBoard();
   });
 
   function showMission() {
+    clearInterval(timeInterval);
+
     optionsContainer.innerHTML = "";
     statusMessage.textContent = "";
     missionTextDisplay.textContent = missions[currentMission].question;
 
-    clearInterval(timeInterval);
-    timeLeft = 10;
+    // TIMER RESET FIX
+    if (selectedCharacter == "GLITCH") {
+      timeLeft = 12;
+    } else {
+      timeLeft = 10;
+    }
+
     updateDashBoard();
 
     timeInterval = setInterval(() => {
       timeLeft--;
+
       updateDashBoard();
 
-      if (timeLeft === 0) {
+      if (timeLeft <= 0) {
         clearInterval(timeInterval);
+
         lives--;
+
         optionsContainer.innerHTML = "";
         missionTextDisplay.textContent = "";
         statusMessage.textContent = "Time Up Agent!";
+
         currentMission++;
+
         updateDashBoard();
 
         setTimeout(() => {
@@ -101,10 +124,29 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }, 1000);
 
-    missions[currentMission].options.forEach((choice) => {
+    let displayedOptions = [...missions[currentMission].options];
+
+    // ORACLE ABILITY
+    if (selectedCharacter == "ORACLE") {
+      const wrongOptions = displayedOptions.filter(
+        (option) => option !== missions[currentMission].answer,
+      );
+
+      const randomWrong =
+        wrongOptions[Math.floor(Math.random() * wrongOptions.length)];
+
+      displayedOptions = displayedOptions.filter(
+        (option) => option !== randomWrong,
+      );
+    }
+
+    displayedOptions.forEach((choice) => {
       const li = document.createElement("li");
+
       li.textContent = choice;
+
       li.addEventListener("click", () => checkAnswer(choice));
+
       optionsContainer.appendChild(li);
     });
   }
@@ -126,6 +168,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     currentMission++;
+
     updateDashBoard();
 
     setTimeout(() => {
@@ -138,67 +181,91 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function updateDashBoard() {
+    agentDisplay.textContent = `Agent : ${selectedCharacter}`;
     livesDisplay.textContent = `Lives : ${lives}`;
     scoreDisplay.textContent = `Score : ${score}`;
-    missionNumDisplay.textContent = `Mission-Number : ${Math.min(currentMission + 1, missions.length)}`;
+
+    missionNumDisplay.textContent = `Mission-Number : ${Math.min(
+      currentMission + 1,
+      missions.length,
+    )}`;
+
     timerDisplay.textContent = `Timer : ${timeLeft}`;
   }
 
   function endGame() {
+    clearInterval(timeInterval);
+
     saveMatchHistory();
 
     gameScreen.classList.add("hidden");
     endScreen.classList.remove("hidden");
+
     showFinalResult();
   }
 
   function showFinalResult() {
     if (lives == 0) {
       finalResult.textContent = "Agent! You couldn't clear the mission.";
-    } else if (lives > 0 && score == missions.length)
-      finalResult.textContent = "Congratulations Agent!You did it!!!";
-    else
+    } else if (lives > 0 && score == missions.length) {
+      finalResult.textContent = "Congratulations Agent! You did it!!!";
+    } else {
       finalResult.textContent =
-        "Agent!!You did well to see the end to challenge but for your skills performance could have been better....";
+        "Agent!! You did well but performance could have been better....";
+    }
 
     finalScore.textContent = `Final Score = ${score} / ${missions.length}`;
   }
+
   restartBtn.addEventListener("click", async () => {
     resetGameVariables();
+
     endScreen.classList.add("hidden");
     gameScreen.classList.remove("hidden");
 
     await fetchMission();
+
     showMission();
     updateDashBoard();
   });
 
   exitBtn.addEventListener("click", () => {
+    clearInterval(timeInterval);
+
     resetGameVariables();
+
     endScreen.classList.add("hidden");
     welcomeScreen.classList.remove("hidden");
   });
 
   function saveMatchHistory() {
     let matchResult;
-    if (score <= missions.length / 3) matchResult = "Defeat";
-    else if (score >= (2 * missions.length) / 3)
+
+    if (score <= missions.length / 3) {
+      matchResult = "Defeat";
+    } else if (score >= (2 * missions.length) / 3) {
       matchResult = "Perfect Victory!!!";
-    else matchResult = "Good Game";
+    } else {
+      matchResult = "Good Game";
+    }
 
     const match = {
+      character: selectedCharacter,
       score: score,
       total: missions.length,
       result: matchResult,
       time: new Date().toLocaleString(),
     };
+
     matchHistory.push(match);
+
     localStorage.setItem("matchHistory", JSON.stringify(matchHistory));
   }
 
   historyBtn.addEventListener("click", () => {
     welcomeScreen.classList.add("hidden");
     historyScreen.classList.remove("hidden");
+
     showHistory();
   });
 
@@ -209,17 +276,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function showHistory() {
     historyList.innerHTML = "";
+
     if (matchHistory.length == 0) {
       historyList.textContent = "No match played yet";
+
       return;
     }
 
     [...matchHistory].reverse().forEach((match) => {
       const historyli = document.createElement("li");
+
       historyli.innerHTML = `
-      Score: ${match.score}/${match.total} | 
-      Result: ${match.result} | 
-      Time: ${match.time}`;
+        Agent: ${match.character} |
+        Score: ${match.score}/${match.total} |
+        Result: ${match.result} |
+        Time: ${match.time}
+      `;
 
       historyList.appendChild(historyli);
     });
@@ -227,10 +299,44 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function resetGameVariables() {
     clearInterval(timeInterval);
+
     score = 0;
-    lives = 3;
     currentMission = 0;
-    timeLeft = 10;
+
+    lives = 3;
+
+    // REAPER ABILITY
+    if (selectedCharacter == "REAPER") {
+      lives = 4;
+    }
+
     statusMessage.textContent = "";
   }
+
+  characterBtn.addEventListener("click", () => {
+    welcomeScreen.classList.add("hidden");
+    characterScreen.classList.remove("hidden");
+  });
+
+  closeCharacterScreen.addEventListener("click", () => {
+    characterScreen.classList.add("hidden");
+    welcomeScreen.classList.remove("hidden");
+  });
+
+  selectCharacterBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+      const card = button.parentElement;
+
+      const characterName = card.querySelector(".character-name").textContent;
+
+      selectedCharacter = characterName;
+
+      characterScreen.classList.add("hidden");
+      welcomeScreen.classList.remove("hidden");
+
+      statusMessage.textContent = `${selectedCharacter} selected`;
+
+      resetGameVariables();
+    });
+  });
 });
